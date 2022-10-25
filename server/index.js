598e2c993bff1640f20745c10e3931cc2f526aa2
faizin <nasrunfaizinkholiq@gmail.com>
2022-10-25 10:48:11 +0700

    init socket io on express

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
-const socketIO = require('socket')(http, {
+const socketIO = require('socket.io')(http, {
     cors: {
         origin: "http://localhost:3000"
     }
